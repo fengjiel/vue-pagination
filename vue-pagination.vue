476eb2f5e710cf1fecfd1f2fbc84453b84db4698
fengjiel <759204840@qq.com>
2017-05-27 16:20:33 +0800

    vue限制了对props数据地直接操作，已修正，更改为传入对象更改对象属性

--- vue-pagination.vue
+++ vue-pagination.vue
@@ -1,81 +1,61 @@
 <template type="text/template" id="template_pagination">
-    <section class="pages-wrap" v-show="total>pagenum">
+    <section class="pages-wrap" v-show="pageInfo.total>pageInfo.pagenum">
         <ul class="pagination clearfix">
-            <li :class="{'disabled': current == 1}"><a href="javascript:;" @click="clickCurrent(1)"> 首页 </a></li>
-            <li :class="{'disabled': current == 1}"><a href="javascript:;" @click="clickCurrent(current - 1)"> 上一页 </a></li> 
-            <li v-for="p in setList" :class="{'active': current == p.val}" >
-                <a href="javascript:;" v-if="current == p.val" :style="{backgroundColor:skin , borderColor:skin}" @click="clickCurrent(p.val)"> {{ p.text }} </a>
+            <li :class="{'disabled': pageInfo.current == 1}"><a href="javascript:;" @click="clickCurrent(1)"> 首页 </a></li>
+            <li :class="{'disabled': pageInfo.current == 1}"><a href="javascript:;" @click="clickCurrent(pageInfo.current - 1)"> 上一页 </a></li>
+            <li v-for="p in setList" :class="{'active': pageInfo.current == p.val}" >
+                <a href="javascript:;" v-if="pageInfo.current == p.val" :style="{backgroundColor:pageInfo.skin , borderColor:pageInfo.skin}" @click="clickCurrent(p.val)"> {{ p.text }} </a>
                 <a href="javascript:;" v-else  @click="clickCurrent(p.val)"> {{ p.text }} </a>
             </li>
-            <li :class="{'disabled': current == page}"><a href="javascript:;" @click="clickCurrent(current + 1)"> 下一页</a></li>
-            <li :class="{'disabled': current == page}"><a href="javascript:;" @click="clickCurrent(page)"> 尾页 </a></li>
+            <li :class="{'disabled': pageInfo.current == pageInfo.page}"><a href="javascript:;" @click="clickCurrent(pageInfo.current + 1)"> 下一页</a></li>
+            <li :class="{'disabled': pageInfo.current == pageInfo.page}"><a href="javascript:;" @click="clickCurrent(pageInfo.page)"> 尾页 </a></li>
         </ul>
     </section>
 </template>
 <script>
 import Vue from 'vue'
 export default {
-    props: {
-        total: {
-            type: Number,
-            default: 0
-        },
-        pagenum: {
-            type: Number,
-            default: 10
-        },
-        current: {
-            type: Number,
-            default: 1
-        },
-        skin: {
-            type: String,
-            default: '#16a086'
-        },
-        pagegroup: {
-            type: Number,
-            default: 5,
-            coerce:function(v){
-                v = v > 0 ? v : 5;
-                return v % 2 === 1 ? v : v + 1;
-            }
-        }
-    },
+    props:['pageInfo'],
     computed: {
         page:function() {
-            return Math.ceil(this.total / this.pagenum);
+            return Math.ceil(this.pageInfo.total / this.pageInfo.pagenum);
         },
         setList:function(){
-            var len = this.page , temp = [], list = [], count = Math.floor(this.pagegroup / 2) ,center = this.current;
-            if( len <= this.pagegroup ){
+            var len = this.page , temp = [], list = [], count = Math.floor(this.pageInfo.pagegroup / 2) ,center = this.pageInfo.current;
+            if( len <= this.pageInfo.pagegroup ){
                 while(len--){ temp.push({text:this.page-len,val:this.page-len});};
                 return temp;
             }
             while(len--){ temp.push(this.page - len);};
-            var idx = temp.indexOf(center);                
-            (idx < count) && ( center = center + count - idx); 
-            (this.current > this.page - count) && ( center = this.page - count);
-            temp = temp.splice(center - count -1, this.pagegroup);                
+            var idx = temp.indexOf(center);
+            (idx < count) && ( center = center + count - idx);
+            (this.pageInfo.current > this.page - count) && ( center = this.page - count);
+            temp = temp.splice(center - count -1, this.pageInfo.pagegroup);
             do {
                 var t = temp.shift();
                 list.push({
                     text: t,
                     val: t
                 });
-            }while(temp.length);                
-            if( this.page > this.pagegroup ){
-                (this.current > count + 1) && list.unshift({ text:'...',val: list[0].val - 1 });
-                (this.current < this.page - count) && list.push({ text:'...',val: list[list.length - 1].val + 1 });
+            }while(temp.length);
+            if( this.page > this.pageInfo.pagegroup ){
+                (this.pageInfo.current > count + 1) && list.unshift({ text:'...',val: list[0].val - 1 });
+                (this.pageInfo.current < this.page - count) && list.push({ text:'...',val: list[list.length - 1].val + 1 });
             }
+            console.log(list)
             return list;
         }
     },
+    created:function (argument) {
+        console.log(this.pageInfo)
+
+    },
     methods: {
         clickCurrent: function(idx) {
-            if( this.current != idx && idx > 0 && idx < this.page + 1) {
-                this.current = idx;
-                this.$emit('change',this.current);                 
-            }                
+            if( this.pageInfo.current != idx && idx > 0 && idx < this.page + 1) {
+                this.pageInfo.current = idx;
+                this.$emit('change',this.pageInfo.current);
+            }
         }
     }
 }
